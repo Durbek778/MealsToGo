04ea4c28d502cd04cc5d0e0563694ff724ef0f4b
Durbek <dergasbaev@gmail.com>
2022-11-09 21:24:05 +0900

    Navugation, bottom-tabbs

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,42 +1,37 @@
-import styled from "styled-components/native";
+import React from "react";
+import styled, { useTheme } from "styled-components/native";
 
-export const Spacer = styled.View``;
-// import { View } from "react-native";
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
 
-// const TopSmall = styled.View`
-//   margin-top: ${(props) => props.theme.space[1]};
-// `;
-// const TopMedium = styled.View`
-//   margin-top: ${(props) => props.theme.space[2]};
-// `;
-// const TopLarge = styled.View`
-//   margin-top: ${(props) => props.theme.space[3]};
-// `;
-// const LeftSmall = styled.View`
-//   margin-left: ${(props) => props.theme.space[1]};
-// `;
+const positionVariant = {
+  top: "marginTop",
+  left: "marginLeft",
+  right: "marginRight",
+  bottom: "marginBottom",
+};
 
-// const LeftMedium = styled.View`
-//   margin-left: ${(props) => props.theme.space[2]};
-// `;
-// const LeftLarge = styled.View`
-//   margin-left: ${(props) => props.theme.space[3]};
-// `;
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
+  return `${property}:${value}`;
+};
 
-// export const Spacer = styled.View``;
-//   if (variant === "top.medium") {
-//     return <TopMedium />;
-//   }
-//   if (variant === "top.large") {
-//     return <TopLarge />;
-//   }
-//   if (variant === "left.small") {
-//     return <LeftSmall />;
-//   }
-//   if (variant === "left.medium") {
-//     return <LeftMedium />;
-//   }
-//   if (variant === "left.large") {
-//     return <LeftLarge />;
-//   }
-//   return <TopSmall />;
+const SpacerView = styled.View`
+  ${({ variant }) => variant}
+`;
+
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
+
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
+};
