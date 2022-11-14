e666ebc7854ae92df7458d2a8da092abfa4182e5
Durbek <dergasbaev@gmail.com>
2022-11-14 19:26:19 +0900

    changes

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,42 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-import { SafeArea } from "../../../components/utility/safe-area.component";
 import styled from "styled-components/native";
+
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 
-const SearchConainer = styled.View`
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
+
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
     padding: 16,
   },
 })``;
 
-export const RestaurantsScreen = () => (
-  <SafeArea>
-    <SearchConainer>
-      <Searchbar />
-    </SearchConainer>
-    <RestaurantList
-      data={[
-        { name: 1 },
-        { name: 2 },
-        { name: 3 },
-        { name: 4 },
-        { name: 5 },
-        { name: 6 },
-        { name: 7 },
-        { name: 8 },
-      ]}
-      renderItem={() => (
-        <Spacer position="bottom" size="large">
-          <RestaurantInfoCard />
-        </Spacer>
-      )}
-      keyExtractor={(item) => item.name}
-    />
-  </SafeArea>
-);
+export const RestaurantsScreen = () => {
+  const restaurantContext = useContext(RestaurantsContext);
+  console.log(restaurantContext);
+  return (
+    <SafeArea>
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={restaurantContext.restaurants}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
+};
