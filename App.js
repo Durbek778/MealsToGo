b80b136f9bc1aac19339804bf892ea99b43f5089
Durbek <dergasbaev@gmail.com>
2022-11-28 20:53:05 +0900

    login sscreen

--- App.js
+++ App.js
@@ -18,12 +18,12 @@ import { FavouritesContextProvider } from "./src/services/favourites/favourites.
 import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmKwzXIQ_7iufDh4U6GyU_4Wc-hyDEnf8",
-  authDomain: "mealstogo-b2612.firebaseapp.com",
-  projectId: "mealstogo-b2612",
-  storageBucket: "mealstogo-b2612.appspot.com",
-  messagingSenderId: "158582890553",
-  appId: "1:158582890553:web:2ef50439fcd80f2ede2de8",
+  apiKey: "AIzaSyB8k-KL6sWy_JVLfLemTxwtye1dYSO3BHs",
+  authDomain: "mealstogo-dd162.firebaseapp.com",
+  projectId: "mealstogo-dd162",
+  storageBucket: "mealstogo-dd162.appspot.com",
+  messagingSenderId: "119348864872",
+  appId: "1:119348864872:web:a2dbe59d7b2d2bf36158dd",
 };
 
 if (!firebase.apps.length) {
--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -19,17 +19,37 @@ export const AuthenticationContextProvider = ({ children }) => {
       })
       .catch((e) => {
         setIsLoading(false);
-        setError(e);
+        setError(e.toString());
+      });
+  };
+
+  const onRegister = (email, password, repeatedPassword) => {
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match");
+      return;
+    }
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
       });
   };
 
   return (
     <AuthenticationContext.Provider
       value={{
+        isAuthenticated: !!user,
         user,
         isLoading,
         error,
         onLogin,
+        onRegister,
       }}
     >
       {children}
